29d6c037532b616a3ea67e44d27fdaa141dc371c
Dmitriy <neposydyakadmytro@gmail.com>
2024-03-18 00:42:26 +0200

    add Login Form

---
+++ src/components/LogInForm.vue
@@ -0,0 +1,46 @@
+<template>
+  <form class="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
+  <div class="mb-4">
+    <label class="block text-gray-700 text-sm font-bold mb-2" for="login">
+      Login
+    </label>
+    <input class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="login" type="text" placeholder="Login">
+  </div>
+  <div class="mb-6">
+    <label class="block text-gray-700 text-sm font-bold mb-2" for="password">
+      Password
+    </label>
+    <input class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline" id="password" type="password" placeholder="******************">
+  </div>
+  <div class="mb-6">
+    <input class="mr-2 leading-tight" type="checkbox" id="remember">
+    <label class="text-sm text-gray-700" for="remember">
+      Remember Me
+    </label>
+  </div>
+  <div class="flex items-center justify-between">
+    <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" type="button">
+      Sign In
+    </button>
+    <a class="inline-block align-baseline font-bold text-sm text-blue-500 hover:text-blue-800" href="#">
+      Forgot Password?
+    </a>
+  </div>
+  <div class="mt-4">
+    <button class="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 w-full rounded focus:outline-none focus:shadow-outline" type="button">
+      Login with Google
+    </button>
+  </div>
+</form>
+</template>
+
+
+<script>
+export default {
+  name: "LogInForm"
+}
+</script>
+
+<style scoped>
+
+</style>
