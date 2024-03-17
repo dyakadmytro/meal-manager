ddc567e646df8d0b9fdc3981a580c0d2fd4a84cc
Dmitriy <neposydyakadmytro@gmail.com>
2024-03-17 23:57:51 +0200

    set up

---
+++ src/views/HomeView.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="container mx-auto">
+    <h1 class="text-slate-700">Home</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "HomeView"
+}
+</script>
+
+<style scoped>
+
+</style>
