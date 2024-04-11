8ad2d754e9f3005f267c643c2eb1c0849b605870
Dmitriy <neposydyakadmytro@gmail.com>
2024-04-11 23:42:26 +0300

    update LogInForm.vue to Element ui

--- src/components/LogInForm.vue
+++ src/components/LogInForm.vue
@@ -1,46 +1,54 @@
 <template>
-  <form class="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
-  <div class="mb-4">
-    <label class="block text-gray-700 text-sm font-bold mb-2" for="login">
-      Login
-    </label>
-    <input class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="login" type="text" placeholder="Login">
-  </div>
-  <div class="mb-6">
-    <label class="block text-gray-700 text-sm font-bold mb-2" for="password">
-      Password
-    </label>
-    <input class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline" id="password" type="password" placeholder="******************">
-  </div>
-  <div class="mb-6">
-    <input class="mr-2 leading-tight" type="checkbox" id="remember">
-    <label class="text-sm text-gray-700" for="remember">
-      Remember Me
-    </label>
-  </div>
-  <div class="flex items-center justify-between">
-    <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" type="button">
-      Sign In
-    </button>
-    <a class="inline-block align-baseline font-bold text-sm text-blue-500 hover:text-blue-800" href="#">
-      Forgot Password?
-    </a>
-  </div>
-  <div class="mt-4">
-    <button class="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 w-full rounded focus:outline-none focus:shadow-outline" type="button">
-      Login with Google
-    </button>
-  </div>
-</form>
+  <el-card class="box-card login-card">
+    <template #header>
+      <div class="card-header">
+        <span>Welcome to MealManager</span>
+      </div>
+    </template>
+    <el-form
+        label-position="right"
+        label-width="80px"
+        :model="loginForm"
+        style="max-width: 460px"
+    >
+      <el-form-item label="Login">
+        <el-input v-model="loginForm.login" />
+      </el-form-item>
+      <el-form-item label="Password">
+        <el-input v-model="loginForm.password" />
+      </el-form-item>
+      <el-form-item>
+        <el-button type="primary" @click="submitForm">Login</el-button>
+      </el-form-item>
+    </el-form>
+  </el-card>
 </template>
 
 
 <script>
 export default {
-  name: "LogInForm"
+  name: "LogInForm",
+  data() {
+    return {
+      loginForm: {
+        login: '',
+        password: ''
+      }
+    }
+  },
+  methods: {
+    submitForm(data) {
+      console.log(data)
+    }
+  }
 }
 </script>
 
 <style scoped>
-
+.login-card {
+  position: relative;
+  top: 10em;
+  margin: auto;
+  width: 25em;
+}
 </style>
--- src/views/LogInView.vue
+++ src/views/LogInView.vue
@@ -1,11 +1,6 @@
 <template>
-  <div class="flex flex-col items-center justify-center min-h-screen login-view-bg">
-    <div class="w-full max-w-xs">
-      <log-in-form />
-      <p class="text-center text-gray-500 text-xs">
-        &copy;2024 Meal Manager. All rights reserved.
-      </p>
-    </div>
+  <div class="login-view-bg">
+    <log-in-form />
   </div>
 
 </template>
@@ -21,6 +16,9 @@ export default {
 
 <style scoped>
 .login-view-bg{
+  position: absolute;
+  width: 100%;
+  height: 100vh;
   background: rgb(231,201,188);
   background: radial-gradient(circle, rgba(231,201,188,0.6153054971988796) 7%, rgba(221,164,202,0.6909357492997199) 49%, rgba(129,150,173,0.6545211834733893) 96%);
 }
